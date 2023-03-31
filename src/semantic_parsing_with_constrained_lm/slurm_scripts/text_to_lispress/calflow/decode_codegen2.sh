be9ac3ed2e3799fe38e9ba3d5d88ef47d4daff92
esteng <elias.stengel@gmail.com>
2023-03-31 15:47:36 -0400

    remove slurm scripts

--- src/semantic_parsing_with_constrained_lm/slurm_scripts/text_to_lispress/calflow/decode_codegen2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python -m semantic_parsing_with_constrained_lm.run_exp \
---config-name semantic_parsing_with_constrained_lm.configs.benchclamp_autoreg_config \
---exp-name-pattern 'codegen-6B_calflow_no_context_low_1_2_dev_eval_constrained_bs_5'
--- src/semantic_parsing_with_constrained_lm/slurm_scripts/text_to_lispress/calflow/test_602.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH -o /home/estengel/semantic_parsing_with_constrained_lm/src/semantic_parsing_with_constrained_lm/logs/602-test.out
-#SBATCH -p ba100
-#SBATCH --gpus=1
-
-echo $CUDA_VISIBLE_DEVICES
-python scripts/602_test.py
