984c6eff7a08e2199a9935b891394a038cd870c0
esteng <elias.stengel@gmail.com>
2023-03-31 12:29:08 -0400

    remote try/except for oom errors

--- src/semantic_parsing_with_constrained_lm/model.py
+++ src/semantic_parsing_with_constrained_lm/model.py
@@ -2,7 +2,6 @@
 # Licensed under the MIT License.
 
 import dataclasses
-import pdb 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Callable, Generic, List, MutableMapping, Optional, Sequence, Tuple
@@ -368,18 +367,12 @@ class BeamSearchSemanticParser(Model[DatumSub], Generic[DatumSub, FullDatumSub, 
         alive using force_decokde, k-best list"""
         max_steps = self.max_steps_fn(test_datum) if self.max_steps_fn else None
         # TODO (elias) add way to get token probs from beam search
-        try:
-            results = await beam_search(
-                self.problem_factory.problem,
-                self.problem_factory.initial(test_datum),
-                self.beam_size,
-                event_listener=LoggingEventListener(self.tokenizer, self.beam_size),
-                max_steps=max_steps,
-            )
-        except RuntimeError:
-        #     # NOTE (elias): adding except to avoid out-of-memory for very long inputs (very rare)
-            print(f"SKIPPING LONG")
-            results = []
+        results = await beam_search(
+            self.problem_factory.problem,
+            self.problem_factory.initial(test_datum),
+            self.beam_size,
+            event_listener=LoggingEventListener(self.tokenizer, self.beam_size),
+        )
 
         return [
             # TODO (elias): add token probs to model result 
