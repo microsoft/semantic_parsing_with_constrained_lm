9a0d16a5444b9fa51f3744b1406e47c3e4ba87ac
esteng <elias.stengel@gmail.com>
2023-03-31 12:30:32 -0400

    update comments

--- src/semantic_parsing_with_constrained_lm/search.py
+++ src/semantic_parsing_with_constrained_lm/search.py
@@ -2,7 +2,6 @@
 # Licensed under the MIT License.
 
 import asyncio
-import pdb 
 import dataclasses
 import gc
 import heapq
@@ -200,11 +199,11 @@ class ConstrainedDecodingProblem(Problem[HS, PSNSub]):
             )
         token_and_logprob_iter: Iterator[Tuple[int, torch.Tensor]]
         if allowed_next is None:
-            # NOTE (elias): this wasn't required before but now is? 
+            # NOTE (elias): move to same device
             indices = torch.arange(next_logprobs.shape[0]).to(next_logprobs.device)
             eligible_logprobs = next_logprobs
         else:
-            # NOTE (elias): this wasn't required before but now is? Error being raised becaause of device mismatch
+            # NOTE (elias): move to same device
             allowed_next = allowed_next.to(next_logprobs.device)
             indices = allowed_next
             eligible_logprobs = next_logprobs[allowed_next]
